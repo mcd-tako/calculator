bf184fd15c7bf4e9cb2f2927d841487e524b3de3
Tiago Cesar <macedocesar.tiago@gmail.com>
2024-08-03 08:44:31 -0300

    calculator app. add function created

---
+++ calculator.go
@@ -0,0 +1,9 @@
+// Package calculator does simple calculations
+// Author: 火の玉
+// Date: Aug 3 2024, 08:40h
+package calculator
+
+// Add takes two numbers and return the result
+func Add(a, b float64) float64 {
+	return a + b
+}
