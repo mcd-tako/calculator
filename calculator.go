3005d5271f1851c6a3e207ff6dba84e9838af0e4
Tiago Macedo <macedocesar.tiago@gmail.com>
2024-08-07 12:58:38 -0300

    division function and both tests

--- calculator.go
+++ calculator.go
@@ -3,6 +3,8 @@
 // Date: Aug 3 2024, 08:40h
 package calculator
 
+import "errors"
+
 // Add takes two numbers and return the result
 func Add(a, b float64) float64 {
 	return a + b
@@ -16,4 +18,11 @@ func Subtract(a, b float64) float64 {
 // Multiply takes two numbers and return the result
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
+	return a / b, nil
 }
